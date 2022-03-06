c64c2134e62e8988b7624c9c7f939b2dde40bbfa
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-03-06 16:50:56 -0500

    Update US2

--- src/test/java/com/vyTrack/tests/base/US2_Oronic_Documentation_page.java
+++ src/test/java/com/vyTrack/tests/base/US2_Oronic_Documentation_page.java
@@ -8,12 +8,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US2_Oronic_Documentation_page {
-
-
     @Test
     public void Oronic_Documentation_page() throws InterruptedException {
         VytrackUtils.loginAsSalesManager();
-
 
         WebElement QuestionMark = Driver.getDriver().findElement(By.xpath("//i[@class='fa-question-circle']"));
         System.out.println("Driver.getDriver().getTitle() = " + Driver.getDriver().getTitle());
@@ -29,9 +26,7 @@ public class US2_Oronic_Documentation_page {
         System.out.println("currentpage = " + currentpage);
 
         Boolean isDisplay = Driver.getDriver().findElement(By.tagName("h1")).isDisplayed();
-
         Assert.assertTrue(isDisplay);
-
     }
 }
 
