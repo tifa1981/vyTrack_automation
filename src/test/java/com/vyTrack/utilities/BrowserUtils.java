7338e81db26d665a42c16d119d7ce5e016d472fd
YanchunZhou <tifa1981@gmail.com>
2022-03-08 16:29:01 -0500

    BrowserUtils and configuration.properties are updated!

--- src/test/java/com/vyTrack/utilities/BrowserUtils.java
+++ src/test/java/com/vyTrack/utilities/BrowserUtils.java
@@ -13,7 +13,7 @@ public class BrowserUtils {
 /*
 This method will accept int(int seconds) and execute thread.sleep for given duration
  */
-    public  void  sleep (int second){
+    public static void  sleep(int second){
         second *=1000;
         try {
             Thread.sleep(second);
